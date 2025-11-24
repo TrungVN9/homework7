b73206889ac07f7b876dcb5156e4ddc280b92c1d
TrungVN9 <trungnvong956@g.ucla.edu>
2025-11-24 00:01:58 -0800

    added part 2 uml diagram and output

--- StateOfVendingMachine.java
+++
@@ -1,8 +0,0 @@
-package com.vendingmachine;
-
-public interface StateOfVendingMachine {
-    void selectSnack(VendingMachine machine, String snackName);
-    void insertMoney(VendingMachine machine, double amount);
-    void dispenseSnack(VendingMachine machine);
-    void cancelTransaction(VendingMachine machine);
-}
